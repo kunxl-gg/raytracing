10fb7f917aa8a1f3efec5c247a2fcda11671425d
kunxl-gg <tiwari.25@iitj.ac.in>
2024-04-13 07:05:41 +0530

    CORE: Initial Commit

---
+++ Makefile
@@ -0,0 +1,28 @@
+BUILD_DIR = ./bin
+APP_NAME = build
+C_FILES = ./src/*.cpp
+ROOT_DIR = $(shell pwd)
+GLAD_FILE = $(ROOT_DIR)/includes/glad.c
+
+CXX := clang++
+CXXFLAGS := -std=c++20 -lglfw3 -lassimp -framework Cocoa -framework OpenGL -framework IOKit
+APP_INCLUDES:= -I$(ROOT_DIR)/includes/
+APP_LINKERS:= -L$(ROOT_DIR)/lib/
+
+# Define targets
+all: $(BUILD_DIR)/$(APP_NAME)
+
+$(BUILD_DIR)/$(APP_NAME): $(C_FILES) $(GLAD_FILE)
+	@mkdir -p $(BUILD_DIR)
+	$(CXX) $(CXXFLAGS) $^ -o $@ $(APP_INCLUDES) $(APP_LINKERS)
+
+run: $(BUILD_DIR)/$(APP_NAME)
+	@./$<
+
+clean:
+	@rm -rf $(BUILD_DIR)
+
+log:
+	@echo $(ROOT_DIR)
+
+.PHONY: all run clean log
---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "Ray Tracing", primaryMonitor, NULL);
+    if (window == nullptr) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        processInput(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
